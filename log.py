b3801b291bb598f6e5f3152f35d669322beac0c9
Amin Jandaghian <a.jandaghian@irisad.net>
2023-09-09 14:44:24 +0330

    init

---
+++ log.py
@@ -0,0 +1,33 @@
+import logging
+
+# Configure the logging module
+logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Decorator function for logging
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        # Log the function call
+        logging.info(f'Calling function: {func.__name__}')
+        
+        # Call the original function and capture its return value
+        result = func(*args, **kwargs)
+        
+        # Log the result, if applicable
+        logging.info(f'Function result: {result}')
+        
+        return result
+    
+    return wrapper
+
+# Usage of the decorator
+@log_function_call
+def add(a, b):
+    return a + b
+
+@log_function_call
+def subtract(a, b):
+    return a - b
+
+# Test the decorated functions
+result1 = add(5, 3)
+result2 = subtract(10, 4)
